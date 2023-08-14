b210f70b73839f5b6b3982b455a5a965dcc13420
Saravana Kumar <44564581+saravanadharmarajan@users.noreply.github.com>
2023-08-14 18:28:52 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,11 @@
+describe('#sum()', function() {
+
+   // test a functionality
+  it('should add numbers', function() {
+    // add an assertion
+    expect(sum(1, 2, 3, 4, 5)).to.equal(15);
+  })
+
+  
+
+})
